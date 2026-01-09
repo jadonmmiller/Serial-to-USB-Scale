06e867fa210b615fdbe854ead3095f3ae78b195a
Jadon Miller <jadonmmiller@gmail.com>
2026-01-08 21:26:00 -0500

    Basic Functionality Complete

--- src/main.cpp
+++ src/main.cpp
@@ -49,118 +49,227 @@ License: The Unlicense
 
 // HID Report Descriptor
 const uint8_t descriptor[] = {
-    0x05, 0x8D,                   // Usage Page (Scale Page)
-    0x09, 0x20,                   // Usage (Scale Device)
-    0xA1, 0x01,                   // Collection (Application)
-    0x09, 0x30,                   //   Usage (Scale Attribute Report)
-    0xA1, 0x02,                   //   Collection (Logical)
-    0x85, 0x01,                   //     Report ID (1)
-    0x75, 0x08,                   //     Report Size (8)
-    0x95, 0x01,                   //     Report Count (1)
-    0x15, 0x01,                   //     Logical Minimum (1)
-    0x09, 0x27,                   //     Usage (Scale Class III English)
-    0xA1, 0x02,                   //     Collection (Logical)
-    0x25, 0x0A,                   //       Logical Maximum (10)
-    0x19, 0x21,                   //       Usage Minimum (Scale Class)
-    0x29, 0x2A,                   //       Usage Maximum (Scale Class Generic)
-    0xB1, 0x00,                   //       Feature (Data,Array,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
-    0xC0,                         //     End Collection
-    0x09, 0x50,                   //     Usage (Weight Unit)
-    0xA1, 0x02,                   //     Collection (Logical)
-    0x25, 0x0C,                   //       Logical Maximum (12)
-    0x19, 0x51,                   //       Usage Minimum (Weight Unit Milligram)
-    0x29, 0x5C,                   //       Usage Maximum (Weight Unit Pound)
-    0xB1, 0x00,                   //       Feature (Data,Array,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
-    0xC0,                         //     End Collection
-    0xC0,                         //   End Collection
-    0x09, 0x31,                   //   Usage (Scale Control Report)
-    0xA1, 0x02,                   //   Collection (Logical)
-    0x85, 0x02,                   //     Report ID (2)
-    0x75, 0x01,                   //     Report Size (1)
-    0x95, 0x02,                   //     Report Count (2)
-    0x15, 0x00,                   //     Logical Minimum (0)
-    0x25, 0x01,                   //     Logical Maximum (1)
-    0x09, 0x81,                   //     Usage (Enforced Zero Return)
-    0x09, 0x80,                   //     Usage (Zero Scale)
-    0x91, 0x02,                   //     Output (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
-    0x75, 0x06,                   //     Report Size (6)
-    0x95, 0x01,                   //     Report Count (1)
-    0x91, 0x03,                   //     Output (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
-    0xC0,                         //   End Collection
-    0x09, 0x32,                   //   Usage (Scale Data Report)
-    0xA1, 0x02,                   //   Collection (Logical)
-    0x85, 0x03,                   //     Report ID (3)
-    0x75, 0x08,                   //     Report Size (8)
-    0x15, 0x01,                   //     Logical Minimum (1)
-    0x09, 0x70,                   //     Usage (Scale Status)
-    0xA1, 0x02,                   //     Collection (Logical)
-    0x25, 0x08,                   //       Logical Maximum (8)
-    0x19, 0x71,                   //       Usage Minimum (Scale Status Fault)
-    0x29, 0x78,                   //       Usage Maximum (Scale Status Requires Rezeroing)
-    0x81, 0x00,                   //       Input (Data,Array,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0xC0,                         //     End Collection
-    0x09, 0x50,                   //     Usage (Weight Unit)
-    0xA1, 0x02,                   //     Collection (Logical)
-    0x25, 0x0C,                   //       Logical Maximum (12)
-    0x19, 0x51,                   //       Usage Minimum (Weight Unit Milligram)
-    0x29, 0x5C,                   //       Usage Maximum (Weight Unit Pound)
-    0x81, 0x00,                   //       Input (Data,Array,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0xC0,                         //     End Collection
-    0x15, 0x81,                   //     Logical Minimum (-127)
-    0x25, 0x7F,                   //     Logical Maximum (127)
-    0x09, 0x41,                   //     Usage (Data Scaling)
-    0x81, 0x02,                   //     Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0x75, 0x10,                   //     Report Size (16)
-    0x15, 0x00,                   //     Logical Minimum (0)
-    0x27, 0xFF, 0xFF, 0x00, 0x00, //     Logical Maximum (65534)
-    0x09, 0x40,                   //     Usage (Data Weight)
-    0x81, 0x02,                   //     Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    0xC0,                         //   End Collection
-    0x09, 0x34,                   //   Usage (Scale Weight Limit Report)
-    0xA1, 0x02,                   //   Collection (Logical)
-    0x85, 0x05,                   //     Report ID (5)
-    0x75, 0x08,                   //     Report Size (8)
-    0x95, 0x01,                   //     Report Count (1)
-    0x09, 0x50,                   //     Usage (Weight Unit)
-    0xA1, 0x02,                   //     Collection (Logical)
-    0x15, 0x01,                   //       Logical Minimum (1)
-    0x25, 0x0C,                   //       Logical Maximum (12)
-    0x19, 0x51,                   //       Usage Minimum (Weight Unit Milligram)
-    0x29, 0x5C,                   //       Usage Maximum (Weight Unit Pound)
-    0xB1, 0x00,                   //       Feature (Data,Array,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
-    0xC0,                         //     End Collection
-    0x15, 0x81,                   //     Logical Minimum (-127)
-    0x25, 0x7F,                   //     Logical Maximum (127)
-    0x09, 0x41,                   //     Usage (Data Scaling)
-    0xB1, 0x02,                   //     Feature (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
-    0x75, 0x10,                   //     Report Size (16)
-    0x15, 0x00,                   //     Logical Minimum (0)
-    0x27, 0xFF, 0xFF, 0x00, 0x00, //     Logical Maximum (65534)
-    0x09, 0x40,                   //     Usage (Data Weight)
-    0xB1, 0x03,                   //     Feature (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
-    0xC0,                         //   End Collection
-    0x09, 0x35,                   //   Usage (Scale Statistics Report)
-    0xA1, 0x02,                   //   Collection (Logical)
-    0x85, 0x06,                   //     Report ID (6)
-    0x75, 0x10,                   //     Report Size (16)
-    0x95, 0x02,                   //     Report Count (2)
-    0x15, 0x00,                   //     Logical Minimum (0)
-    0x27, 0xFF, 0xFF, 0x00, 0x00, //     Logical Maximum (65534)
-    0x09, 0x60,                   //     Usage (Calibration Count)
-    0x09, 0x61,                   //     Usage (Re-Zero Count)
-    0xB1, 0x03,                   //     Feature (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
-    0xC0,                         //   End Collection
-    0x09, 0x30,                   //   Usage (Scale Attribute Report)
-    0xA1, 0x02,                   //   Collection (Logical)
-    0x85, 0x07,                   //     Report ID (7)
-    0x75, 0x08,                   //     Report Size (8)
-    0x95, 0x0F,                   //     Report Count (15)
-    0x15, 0x00,                   //     Logical Minimum (0)
-    0x25, 0x7F,                   //     Logical Maximum (127)
-    0x09, 0x40,                   //     Usage (Data Weight)
-    0xB1, 0x02,                   //     Feature (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
-    0xC0,                         //   End Collection
-    0xC0,                         // End Collection
+    0x05,
+    0x8D, // Usage Page (Scale Page)
+    0x09,
+    0x20, // Usage (Scale Device)
+    0xA1,
+    0x01, // Collection (Application)
+    0x09,
+    0x30, //   Usage (Scale Attribute Report)
+    0xA1,
+    0x02, //   Collection (Logical)
+    0x85,
+    0x01, //     Report ID (1)
+    0x75,
+    0x08, //     Report Size (8)
+    0x95,
+    0x01, //     Report Count (1)
+    0x15,
+    0x01, //     Logical Minimum (1)
+    0x09,
+    0x27, //     Usage (Scale Class III English)
+    0xA1,
+    0x02, //     Collection (Logical)
+    0x25,
+    0x0A, //       Logical Maximum (10)
+    0x19,
+    0x21, //       Usage Minimum (Scale Class)
+    0x29,
+    0x2A, //       Usage Maximum (Scale Class Generic)
+    0xB1,
+    0x00, //       Feature (Data,Array,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
+    0xC0, //     End Collection
+    0x09,
+    0x50, //     Usage (Weight Unit)
+    0xA1,
+    0x02, //     Collection (Logical)
+    0x25,
+    0x0C, //       Logical Maximum (12)
+    0x19,
+    0x51, //       Usage Minimum (Weight Unit Milligram)
+    0x29,
+    0x5C, //       Usage Maximum (Weight Unit Pound)
+    0xB1,
+    0x00, //       Feature (Data,Array,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
+    0xC0, //     End Collection
+    0xC0, //   End Collection
+    0x09,
+    0x31, //   Usage (Scale Control Report)
+    0xA1,
+    0x02, //   Collection (Logical)
+    0x85,
+    0x02, //     Report ID (2)
+    0x75,
+    0x01, //     Report Size (1)
+    0x95,
+    0x02, //     Report Count (2)
+    0x15,
+    0x00, //     Logical Minimum (0)
+    0x25,
+    0x01, //     Logical Maximum (1)
+    0x09,
+    0x81, //     Usage (Enforced Zero Return)
+    0x09,
+    0x80, //     Usage (Zero Scale)
+    0x91,
+    0x02, //     Output (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
+    0x75,
+    0x06, //     Report Size (6)
+    0x95,
+    0x01, //     Report Count (1)
+    0x91,
+    0x03, //     Output (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
+    0xC0, //   End Collection
+    0x09,
+    0x32, //   Usage (Scale Data Report)
+    0xA1,
+    0x02, //   Collection (Logical)
+    0x85,
+    0x03, //     Report ID (3)
+    0x75,
+    0x08, //     Report Size (8)
+    0x15,
+    0x01, //     Logical Minimum (1)
+    0x09,
+    0x70, //     Usage (Scale Status)
+    0xA1,
+    0x02, //     Collection (Logical)
+    0x25,
+    0x08, //       Logical Maximum (8)
+    0x19,
+    0x71, //       Usage Minimum (Scale Status Fault)
+    0x29,
+    0x78, //       Usage Maximum (Scale Status Requires Rezeroing)
+    0x81,
+    0x00, //       Input (Data,Array,Abs,No Wrap,Linear,Preferred State,No Null Position)
+    0xC0, //     End Collection
+    0x09,
+    0x50, //     Usage (Weight Unit)
+    0xA1,
+    0x02, //     Collection (Logical)
+    0x25,
+    0x0C, //       Logical Maximum (12)
+    0x19,
+    0x51, //       Usage Minimum (Weight Unit Milligram)
+    0x29,
+    0x5C, //       Usage Maximum (Weight Unit Pound)
+    0x81,
+    0x00, //       Input (Data,Array,Abs,No Wrap,Linear,Preferred State,No Null Position)
+    0xC0, //     End Collection
+    0x15,
+    0x81, //     Logical Minimum (-127)
+    0x25,
+    0x7F, //     Logical Maximum (127)
+    0x09,
+    0x41, //     Usage (Data Scaling)
+    0x81,
+    0x02, //     Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+    0x75,
+    0x10, //     Report Size (16)
+    0x15,
+    0x00, //     Logical Minimum (0)
+    0x27,
+    0xFF,
+    0xFF,
+    0x00,
+    0x00, //     Logical Maximum (65534)
+    0x09,
+    0x40, //     Usage (Data Weight)
+    0x81,
+    0x02, //     Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+    0xC0, //   End Collection
+    0x09,
+    0x34, //   Usage (Scale Weight Limit Report)
+    0xA1,
+    0x02, //   Collection (Logical)
+    0x85,
+    0x05, //     Report ID (5)
+    0x75,
+    0x08, //     Report Size (8)
+    0x95,
+    0x01, //     Report Count (1)
+    0x09,
+    0x50, //     Usage (Weight Unit)
+    0xA1,
+    0x02, //     Collection (Logical)
+    0x15,
+    0x01, //       Logical Minimum (1)
+    0x25,
+    0x0C, //       Logical Maximum (12)
+    0x19,
+    0x51, //       Usage Minimum (Weight Unit Milligram)
+    0x29,
+    0x5C, //       Usage Maximum (Weight Unit Pound)
+    0xB1,
+    0x00, //       Feature (Data,Array,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
+    0xC0, //     End Collection
+    0x15,
+    0x81, //     Logical Minimum (-127)
+    0x25,
+    0x7F, //     Logical Maximum (127)
+    0x09,
+    0x41, //     Usage (Data Scaling)
+    0xB1,
+    0x02, //     Feature (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
+    0x75,
+    0x10, //     Report Size (16)
+    0x15,
+    0x00, //     Logical Minimum (0)
+    0x27,
+    0xFF,
+    0xFF,
+    0x00,
+    0x00, //     Logical Maximum (65534)
+    0x09,
+    0x40, //     Usage (Data Weight)
+    0xB1,
+    0x03, //     Feature (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
+    0xC0, //   End Collection
+    0x09,
+    0x35, //   Usage (Scale Statistics Report)
+    0xA1,
+    0x02, //   Collection (Logical)
+    0x85,
+    0x06, //     Report ID (6)
+    0x75,
+    0x10, //     Report Size (16)
+    0x95,
+    0x02, //     Report Count (2)
+    0x15,
+    0x00, //     Logical Minimum (0)
+    0x27,
+    0xFF,
+    0xFF,
+    0x00,
+    0x00, //     Logical Maximum (65534)
+    0x09,
+    0x60, //     Usage (Calibration Count)
+    0x09,
+    0x61, //     Usage (Re-Zero Count)
+    0xB1,
+    0x03, //     Feature (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
+    0xC0, //   End Collection
+    0x09,
+    0x30, //   Usage (Scale Attribute Report)
+    0xA1,
+    0x02, //   Collection (Logical)
+    0x85,
+    0x07, //     Report ID (7)
+    0x75,
+    0x08, //     Report Size (8)
+    0x95,
+    0x0F, //     Report Count (15)
+    0x15,
+    0x00, //     Logical Minimum (0)
+    0x25,
+    0x7F, //     Logical Maximum (127)
+    0x09,
+    0x40, //     Usage (Data Weight)
+    0xB1,
+    0x02, //     Feature (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
+    0xC0, //   End Collection
+    0xC0, // End Collection
 };
 
 // ---------- Structs and Enums ----------
@@ -216,13 +325,13 @@ int statusLEDHue = 0; // Tracks hue for animations
 #define ENABLE_DEBUG
 #ifdef ENABLE_DEBUG
 #define DEBUG_PORT Serial
-#define DEBUG_BAUD 9600
+#define DEBUG_BAUD 115200
 // #define DEBUG_HID
 #define DEBUG_SCALES
 #ifdef DEBUG_SCALES
 #define DEBUG_SCALES_RECEIVE
-#define DEBUG_SCALES_VERIFY
-#define DEBUG_SCALES_PARSE
+// #define DEBUG_SCALES_VERIFY
+// #define DEBUG_SCALES_PARSE
 #endif
 #endif
 
@@ -245,10 +354,9 @@ scalesProfile_t scalesProfile[1] = {
         0,                                                                             // Index of Weight Value in Response
         "GROSS WT:     0.00 lb\r\nCOUNT:            0\r\nPIECE WT: -------- lb\r\n\n", // Response Format in Pounds
         "GROSS WT:     0.00 kg\r\nCOUNT:            0\r\nPIECE WT: -------- kg\r\n\n", // Response Format in Kilograms
-        // Response mask, zeros indicate data that never changes, and ones indicate variables
-        "00000000001111111100000000000011111111111100000000000011111111000000",
-        100.00, // Maximum Weight
-        -50.00  // Minimum Weight
+        "00000000001111111100000000000011111111111100000000000011111111000000",        // Response mask, zeros indicate data that never changes, and ones indicate variables
+        -50.00,                                                                        // Minimum Weight
+        100.00,                                                                        // Maximum Weight
     }};
 
 // ---------- Runtime Variables ----------
@@ -356,8 +464,11 @@ void statusLEDUpdate()
   }
   else if (scalesConnected)
   {
-    statusLED[0] = scalesProfile[activeScalesProfile].LEDColor;
-    FastLED.show();
+    if (statusLED[0] != scalesProfile[activeScalesProfile].LEDColor)
+    {
+      statusLED[0] = scalesProfile[activeScalesProfile].LEDColor;
+      FastLED.show();
+    }
   }
 }
 #endif
@@ -484,7 +595,7 @@ void scalesReceive()
     DEBUG_PORT.println("Receiving '" + String(char(SCALES_PORT.peek())) + "'");
 #endif
     receivedData[bufferIndex] = SCALES_PORT.read();
-    receivedData[bufferIndex + 1] = '\0';       // Terminate the string
+    receivedData[bufferIndex + 1] = '\0';           // Terminate the string
     if (bufferIndex < SCALES_MAX_RESPONSE_SIZE - 1) // Check for buffer overflows
     {
       bufferIndex++;
@@ -559,7 +670,7 @@ void scalesParse(char *data)
   }
 
   // Start parsing the data
-  float parsedNumbers[scalesProfile[0].numResponseValues + 1] = {0};
+  float parsedNumbers[scalesProfile[activeScalesProfile].numResponseValues + 1] = {0};
 
 #ifdef DEBUG_SCALES_PARSE
   Serial.println("Parsing Data");
@@ -594,7 +705,7 @@ void scalesParse(char *data)
       }
 
       // Get ready to receive the next value
-      if (numberIndex < scalesProfile[0].numResponseValues)
+      if (numberIndex < scalesProfile[activeScalesProfile].numResponseValues)
       {
         numberIndex++;
       }
@@ -609,11 +720,11 @@ void scalesParse(char *data)
   } // Finished looping through data
 
 #ifdef DEBUG_SCALES
-  Serial.println("Weight Parsed: " + String(parsedNumbers[scalesProfile[0].responseWeightValueIndex]));
+  Serial.println("Weight Parsed: " + String(parsedNumbers[scalesProfile[activeScalesProfile].responseWeightValueIndex]));
 #endif
 
   // Convert and save the weight
-  scalesCalcWeight(parsedNumbers[scalesProfile[0].responseWeightValueIndex], responseUnit);
+  scalesCalcWeight(parsedNumbers[scalesProfile[activeScalesProfile].responseWeightValueIndex], responseUnit);
 }
 
 // Converts the weight from the scales to what the computer wants
@@ -635,7 +746,7 @@ void scalesCalcWeight(float raw, unit_t unit)
   }
 
   // Check if the weight is within scales limits
-  if (weightLB >= scalesProfile[0].minWeight && weightLB <= scalesProfile[0].maxWeight)
+  if (weightLB >= scalesProfile[activeScalesProfile].minWeight && weightLB <= scalesProfile[activeScalesProfile].maxWeight)
   {
 #ifdef DEBUG_SCALES
     Serial.println("Weight Valid");
@@ -727,23 +838,6 @@ bool isNumeric(char c)
     break;
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 /*
 // the setup function runs once when you press reset or power the board
